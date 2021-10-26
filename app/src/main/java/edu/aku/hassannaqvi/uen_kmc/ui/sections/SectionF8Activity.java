fa664f23ba0fbce3e6a965bd625b6fbaeb0f66d6
ahs1n <ahsanpervez74@gmail.com>
2021-10-26 10:59:05 +0500

    Add F8

---
+++ app/src/main/java/edu/aku/hassannaqvi/uen_kmc/ui/sections/SectionF8Activity.java
@@ -0,0 +1,27 @@
+package edu.aku.hassannaqvi.uen_kmc.ui.sections;
+
+import static edu.aku.hassannaqvi.uen_kmc.core.MainApp.form;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import edu.aku.hassannaqvi.uen_kmc.R;
+import edu.aku.hassannaqvi.uen_kmc.database.DatabaseHelper;
+import edu.aku.hassannaqvi.uen_kmc.databinding.ActivitySectionF8Binding;
+
+public class SectionF8Activity extends AppCompatActivity {
+    private static final String TAG = "SectionF8Activity";
+    ActivitySectionF8Binding bi;
+    private DatabaseHelper db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        bi = DataBindingUtil.setContentView(this, R.layout.activity_section_f8);
+        bi.setCallback(this);
+        bi.setForm(form);
+
+    }
+}
