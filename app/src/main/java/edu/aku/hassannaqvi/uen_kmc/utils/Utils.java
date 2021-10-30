2cf86045bd804a8ce2e8e0b9171f10a3a774abc1
ahs1n <ahsanpervez74@gmail.com>
2021-10-30 15:36:31 +0500

    *commit

--- app/src/main/java/edu/aku/hassannaqvi/uen_kmc/utils/Utils.java
+++ app/src/main/java/edu/aku/hassannaqvi/uen_kmc/utils/Utils.java
@@ -26,38 +26,4 @@ public class Utils {
             }
         }
     }
-
-/*    public static void setCustomDatabaseFiles(Context context) {
-        if (BuildConfig.DEBUG) {
-            try {
-                Class<?> debugDB = Class.forName("com.amitshekhar.DebugDB");
-                Class[] argTypes = new Class[]{HashMap.class};
-                Method setCustomDatabaseFiles = debugDB.getMethod("setCustomDatabaseFiles", argTypes);
-                HashMap<String, Pair<File, String>> customDatabaseFiles = new HashMap<>();
-                // set your custom database files
-                customDatabaseFiles.put(ExtTestDBHelper.DATABASE_NAME,
-                        new Pair<>(new File(context.getFilesDir() + "/" + ExtTestDBHelper.DIR_NAME +
-                                "/" + ExtTestDBHelper.DATABASE_NAME), ""));
-                setCustomDatabaseFiles.invoke(null, customDatabaseFiles);
-            } catch (Exception ignore) {
-
-            }
-        }
-    }
-
-    public static void setInMemoryRoomDatabases(SupportSQLiteDatabase... database) {
-        if (BuildConfig.DEBUG) {
-            try {
-                Class<?> debugDB = Class.forName("com.amitshekhar.DebugDB");
-                Class[] argTypes = new Class[]{HashMap.class};
-                HashMap<String, SupportSQLiteDatabase> inMemoryDatabases = new HashMap<>();
-                // set your inMemory databases
-                inMemoryDatabases.put("InMemoryOne.db", database[0]);
-                Method setRoomInMemoryDatabase = debugDB.getMethod("setInMemoryRoomDatabases", argTypes);
-                setRoomInMemoryDatabase.invoke(null, inMemoryDatabases);
-            } catch (Exception ignore) {
-
-            }
-        }
-    }*/
 }
