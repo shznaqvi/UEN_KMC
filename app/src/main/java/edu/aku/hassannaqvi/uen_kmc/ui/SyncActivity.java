473bd9c16e6f17aaedead50ae1d10092540aa2d6
ahs1n <ahsanpervez74@gmail.com>
2022-01-15 14:06:48 +0500

    *commit

--- app/src/main/java/edu/aku/hassannaqvi/uen_kmc/ui/SyncActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/uen_kmc/ui/SyncActivity.java
@@ -188,11 +188,11 @@ public class SyncActivity extends AppCompatActivity {
                     filter = " enabled = '1' ";
 
                     downloadTables.add(new SyncModel(UsersTable.TABLE_NAME, select, filter));
+                    downloadTables.add(new SyncModel(TableDistricts.TABLE_NAME, select, filter));
                     downloadTables.add(new SyncModel(VersionTable.TABLE_NAME));
                 } else {
                     select = " * ";
                     filter = " col_flag is null AND dist_id = '" + MainApp.user.getDist_id() + "' ";
-                    downloadTables.add(new SyncModel(TableDistricts.TABLE_NAME, select, filter));
                     downloadTables.add(new SyncModel(TableTehsil.TABLE_NAME, select, filter));
                     downloadTables.add(new SyncModel(TableHealthFacilities.TABLE_NAME, select, filter));
                 }
