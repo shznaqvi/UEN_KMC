c0563e9840aa987d58e3bf2afcb357fe5916a8dc
ahs1n <ahsanpervez74@gmail.com>
2022-01-18 10:29:21 +0500

    *commit

--- app/src/main/java/edu/aku/hassannaqvi/uen_kmc/core/MainApp.java
+++ app/src/main/java/edu/aku/hassannaqvi/uen_kmc/core/MainApp.java
@@ -35,7 +35,7 @@ public class MainApp extends Application {
     public static final String _IP = "https://vcoe1.aku.edu";// .LIVE server
     //    public static final String _IP = "https://cls-pae-fp51764";// .TEST server
     //public static final String _IP = "http://43.245.131.159:8080";// .TEST server
-    public static final String _HOST_URL = MainApp._IP + "/uen_kmc/api/";// .TEST server;
+    public static final String _HOST_URL = MainApp._IP + "/uen_ph2/api/";// .TEST server;
     public static final String _SERVER_URL = "syncenc.php";
     public static final String _SERVER_GET_URL = "getDataenc.php";
     public static final String _PHOTO_UPLOAD_URL = _HOST_URL + "uploads.php";
