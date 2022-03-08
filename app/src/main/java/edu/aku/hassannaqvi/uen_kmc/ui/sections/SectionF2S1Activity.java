a9fd766c1c0b9b6d865e67f255bd55932cfb30a6
Muhammad Hussain <ahsanpervez74@gmail.com>
2022-03-08 18:01:28 +0500

    added module 2

---
+++ app/src/main/java/edu/aku/hassannaqvi/uen_kmc/ui/sections/SectionF2S1Activity.java
@@ -0,0 +1,28 @@
+package edu.aku.hassannaqvi.uen_kmc.ui.sections;
+
+import static edu.aku.hassannaqvi.uen_kmc.core.MainApp.form;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import edu.aku.hassannaqvi.uen_kmc.R;
+import edu.aku.hassannaqvi.uen_kmc.core.MainApp;
+import edu.aku.hassannaqvi.uen_kmc.database.DatabaseHelper;
+import edu.aku.hassannaqvi.uen_kmc.databinding.ActivitySectionF2S1Binding;
+
+public class SectionF2S1Activity extends AppCompatActivity {
+    private static final String TAG = "SectionF2S1Activity";
+    ActivitySectionF2S1Binding bi;
+    private DatabaseHelper db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        bi = DataBindingUtil.setContentView(this, R.layout.activity_section_f2_s1);
+        bi.setForm(form);
+//        setSupportActionBar(bi.toolbar);
+        db = MainApp.appInfo.dbHelper;
+    }
+}
---
+++ app/src/main/java/edu/aku/hassannaqvi/uen_kmc/ui/sections/SectionF2S3Activity.java
@@ -0,0 +1,28 @@
+package edu.aku.hassannaqvi.uen_kmc.ui.sections;
+
+import static edu.aku.hassannaqvi.uen_kmc.core.MainApp.form;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import edu.aku.hassannaqvi.uen_kmc.R;
+import edu.aku.hassannaqvi.uen_kmc.core.MainApp;
+import edu.aku.hassannaqvi.uen_kmc.database.DatabaseHelper;
+import edu.aku.hassannaqvi.uen_kmc.databinding.ActivitySectionF2S3Binding;
+
+public class SectionF2S3Activity extends AppCompatActivity {
+    private static final String TAG = "SectionF2S3Activity";
+    ActivitySectionF2S3Binding bi;
+    private DatabaseHelper db;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        bi = DataBindingUtil.setContentView(this, R.layout.activity_section_f2_s3);
+        bi.setForm(form);
+//        setSupportActionBar(bi.toolbar);
+        db = MainApp.appInfo.dbHelper;
+    }
+}
