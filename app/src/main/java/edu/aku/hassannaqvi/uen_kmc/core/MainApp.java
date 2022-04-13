465b65a3e736d7bff6e2172fff92708a308ffd9c
Muhammad Hussain <ahsanpervez74@gmail.com>
2022-04-13 11:21:50 +0500

    *commit

--- app/src/main/java/edu/aku/hassannaqvi/uen_kmc/core/MainApp.java
+++ app/src/main/java/edu/aku/hassannaqvi/uen_kmc/core/MainApp.java
@@ -38,11 +38,11 @@ public class MainApp extends Application {
     public static final String _IP = "https://vcoe1.aku.edu";// .LIVE server
     //    public static final String _IP = "https://cls-pae-fp51764";// .TEST server
     //public static final String _IP = "http://43.245.131.159:8080";// .TEST server
-    public static final String _HOST_URL = MainApp._IP + "/uen_kmc/api/";// .TEST server;
+    public static final String _HOST_URL = MainApp._IP + "/uen_ph2/api/";// .TEST server;
     public static final String _SERVER_URL = "syncenc.php";
     public static final String _SERVER_GET_URL = "getDataenc.php";
     public static final String _PHOTO_UPLOAD_URL = _HOST_URL + "uploads.php";
-    public static final String _UPDATE_URL = MainApp._IP + "/uen_kmc/app/kmc";
+    public static final String _UPDATE_URL = MainApp._IP + "/uen_ph2/app/kmc";
     public static final String _EMPTY_ = "";
     private static final String TAG = "MainApp";
     public static String IBAHC = "";
